33e50b7d13c16cd8ef0034a85b5c01a612f1488a
patrick.trias <patrick.trias@gmail.com>
2019-07-03 02:13:04 -0400

    adding php extension

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Patrick Trias!';
+echo 'lab1 = http://ptrias-cst336.herokuapp.com/labs/lab1/';
 
 ?>
 </body>
