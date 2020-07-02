60efc3a2d44fdc3e9c7a35aec68c0d3661937336
ptrias <ptrias@csumb.edu>
2020-07-02 00:32:33 +0000

    lab2

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 // then click the URL that is emitted to the Output tab of the console
 
 echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab1/">Lab1</a>';
+<br>
 echo '<a href="http://ptrias-cst336.herokuapp.com/homework/hw1/index.html">HW 1</a>';
 ?>
 </body>
