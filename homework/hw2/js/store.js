1cb98f5ce8ca7ce7b9abba8cb5dddcb3c56dde1e
ptrias <ptrias@csumb.edu>
2020-07-11 14:06:14 +0000

    hw2

--- homework/hw2/js/store.js
+++ homework/hw2/js/store.js
@@ -3,7 +3,8 @@ console.log(removeCartItemButtons)
 
 for (var i = 0; i < removeCartItemButtons; i++){
     var button = removeCartItemButtons[i]
-    button.addEventListener('click', function(){
-        console.log('clicked')
+    button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
     })
 }
