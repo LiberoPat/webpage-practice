3a92bc5a444bc270f0ac37d484c5f2ae74167364
ptrias <ptrias@csumb.edu>
2020-07-11 13:56:16 +0000

    hw2

--- homework/hw2/js/store.js
+++ homework/hw2/js/store.js
@@ -1,2 +1,9 @@
 var removeCartItemButtons = document.getElementsByClassName('btn-danger')
 console.log(removeCartItemButtons)
+
+for (var i = 0; i < removeCartItemButtons; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(){
+        console.log('clicked')
+    })
+}
