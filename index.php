0defdbbddd00fc4959c5b2d9d275e615a5a18263
ptrias <ptrias@csumb.edu>
2020-07-31 05:50:02 +0000

    index.php

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab1/">Lab1</a>';
+echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab1/">Website Portfolio in PHP</a>';
 echo"<br>";
-echo '<a href="http://ptrias-cst336.herokuapp.com/homework/hw1/index.html">HW 1</a>';
+echo '<a href="http://ptrias-cst336.herokuapp.com/homework/hw1/index.html">Simple website about SCRUM and Agile process</a>';
 echo"<br>";
-echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab2/">Lab 2</a>';
-
-
+echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab2/">Quiz using JQuery</a>';
+echo"<br>";
+echo '<a href="https://patr-lab4.herokuapp.com">Solar System Website Built RESTfully with Javascript, JQuery, node.js and </a>';
+echo"<br>";
+echo '<a href="http://ptrias-cst336.herokuapp.com/homework/hw3/index.html">Search for Walmart Product using their Walmart Labs API </a>';
+echo"<br>";
+echo '<a href="https://patr-lab5.herokuapp.com">Instagram replica using copyright free photos API</a>';
 ?>
 </body>
 </html>
