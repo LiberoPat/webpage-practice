efee8dde6e0ce339885a51763bb496f264da04d0
ptrias <ptrias@csumb.edu>
2020-07-31 05:52:31 +0000

    index.php

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab1/">Website Portfolio in PHP</a>';
-echo"<br>";
-echo '<a href="http://ptrias-cst336.herokuapp.com/homework/hw1/index.html">Simple website about SCRUM and Agile process</a>';
-echo"<br>";
-echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab2/">Quiz using JQuery</a>';
-echo"<br>";
-echo '<a href="https://patr-lab4.herokuapp.com">Solar System Website Built RESTfully with Javascript, JQuery, node.js and </a>';
-echo"<br>";
-echo '<a href="http://ptrias-cst336.herokuapp.com/homework/hw3/index.html">Search for Walmart Product using their Walmart Labs API </a>';
-echo"<br>";
-echo '<a href="https://patr-lab5.herokuapp.com">Instagram replica using copyright free photos API</a>';
-?>
-</body>
+<html>
+    
+    <main>
+        <h1>Cool Images</h1>
+    </main>
+    <body>
+        <?php
+        // A simple web site in Cloud9 that runs through Apache
+        // Press the 'Run' button on the top to start the web server,
+        // then click the URL that is emitted to the Output tab of the console
+        
+        echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab1/">Website Portfolio in PHP</a>';
+        echo"<br>";
+        echo '<a href="http://ptrias-cst336.herokuapp.com/homework/hw1/index.html">Simple website about SCRUM and Agile process</a>';
+        echo"<br>";
+        echo '<a href="http://ptrias-cst336.herokuapp.com/labs/lab2/">Quiz using JQuery</a>';
+        echo"<br>";
+        echo '<a href="https://patr-lab4.herokuapp.com">Solar System Website Built RESTfully with Javascript, JQuery, node.js and </a>';
+        echo"<br>";
+        echo '<a href="http://ptrias-cst336.herokuapp.com/homework/hw3/index.html">Search for Walmart Product using their Walmart Labs API </a>';
+        echo"<br>";
+        echo '<a href="https://patr-lab5.herokuapp.com">Instagram replica using copyright free photos API</a>';
+        ?>
+    </body>
 </html>
